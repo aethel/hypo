009014c08b92cef492d5058a8d4c0f1a3dea702b
Hussariush <aethelbrihte@gmail.com>
2020-10-31 17:42:04 +0100

    Form markup

---
+++ src/components/Form/Form.vue
@@ -0,0 +1,53 @@
+<template>
+  <form>
+      <legend>Mortgage Calculator</legend>
+      <ol>
+          <li>
+              <label for="commission">Real Estate Commission</label>
+              <input required type="checkbox" id="commission" checked />          
+            </li>
+          <li>
+              <label for="purchasePrice">Property Purchase Price</label>
+              <input type="number" id="purchasePrice" inputmode="number" value="0" />   
+          </li>
+          <li>
+              <label for="totalSavings">Total Savings</label>
+              <input required type="number" id="totalSavings" inputmode="number" value="0" />   
+          </li>
+          <li>
+              <label for="repayment">Annual Repayment</label>
+              <input required type="number" id="repayment" value="0" />          
+            </li>
+          <li>
+              <button type="submit">Calculate</button>   
+          </li>
+      </ol>
+  </form>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <Form/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Form from "@/components/Form/Form.vue";
+
 
 @Component({
   components: {
-    HelloWorld
+    // HelloWorld,
+    Form
   }
 })
 export default class Home extends Vue {}
