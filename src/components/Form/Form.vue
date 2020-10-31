446ae111cd5c992cb87b51d1a2e51d86b5f601ac
Hussariush <aethelbrihte@gmail.com>
2020-10-31 19:16:22 +0100

    Form data collection

--- src/components/Form/Form.vue
+++ src/components/Form/Form.vue
@@ -1,27 +1,52 @@
 <template>
   <form>
-      <legend>Mortgage Calculator</legend>
-      <ol>
-          <li>
-              <label for="commission">Real Estate Commission</label>
-              <input required type="checkbox" id="commission" checked />          
-            </li>
-          <li>
-              <label for="purchasePrice">Property Purchase Price</label>
-              <input type="number" id="purchasePrice" inputmode="number" value="0" />   
-          </li>
-          <li>
-              <label for="totalSavings">Total Savings</label>
-              <input required type="number" id="totalSavings" inputmode="number" value="0" />   
-          </li>
-          <li>
-              <label for="repayment">Annual Repayment</label>
-              <input required type="number" id="repayment" value="0" />          
-            </li>
-          <li>
-              <button type="submit">Calculate</button>   
-          </li>
-      </ol>
+    <legend>Mortgage Calculator</legend>
+    <ol>
+      <li>
+        <label for="commission">Real Estate Commission</label>
+        <input
+          required
+          type="checkbox"
+          v-model.lazy="mortgageCalculations.commission"
+          id="commission"
+          checked="mortgageCalculations.comission"
+        />
+      </li>
+      <li>
+        <label for="purchasePrice">Property Purchase Price</label>
+        <input
+          type="number"
+          id="purchasePrice"
+          v-modelnumber.lazy="mortgageCalculations.purchasePrice"
+          inputmode="number"
+          value="mortgageCalculations.purchasePrice"
+        />
+      </li>
+      <li>
+        <label for="totalSavings">Total Savings</label>
+        <input
+          required
+          type="number"
+          id="totalSavings"
+          v-model.number.lazy="mortgageCalculations.totalSavings"
+          inputmode="number"
+          value="mortgageCalculations.totalSavings"
+        />
+      </li>
+      <li>
+        <label for="repayment">Annual Repayment</label>
+        <input
+          required
+          type="number"
+          v-model.number.lazy="mortgageCalculations.repayment"
+          id="repayment"
+          value="mortgageCalculations.repayment"
+        />
+      </li>
+      <li>
+        <button type="button" @click="recalculate">Calculate</button>
+      </li>
+    </ol>
   </form>
 </template>
 
@@ -29,8 +54,20 @@
 import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component
-export default class HelloWorld extends Vue {
+export default class Form extends Vue {
   @Prop() private msg!: string;
+  @Prop() private mortgageCalculations: { [key: string]: any } = {
+    commission: false,
+    repayment: 0,
+    totalSavings: 0,
+    purchasePrice: 0
+  };
+  get calculations() {
+    return this.mortgageCalculations;
+  }
+  recalculate = () => {
+    console.log(this.calculations);
+  };
 }
 </script>
 
